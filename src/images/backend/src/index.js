6b258345cfb01601cb2268de1977e5f2755fb180
Florian Bösl <f.boesl@oth-aw.de>
2022-05-21 14:48:16 +0200

    socket.io +,  Beginn Websockets zu impl.
    Former-commit-id: 6981b9f64bac317a5e8660fa8069805be401f6a1

--- src/images/backend/src/index.js
+++ src/images/backend/src/index.js
@@ -4,9 +4,31 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 
+const path = require('path');
+
+//const http = require('http');     // http-Module - notwendig?
+const io = require('socket.io');
+
+var explGuy = require('./explGuy'); // Import the "Backend game logic file".
+
 app.use(cors());
 app.use(express.json());
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 })
+
+
+/*
+
+//const server = http.createServer(app).listen(port);;  // Create new http-Server - notwendig?
+io.listen(app);  // attach the  Socket.IO server to the (http) server
+
+// Listen for Socket.IO Connections. Once connected, start the game logic.
+io.sockets.on('connection', function (socket) {   // io.on geht genauso
+
+  explGuy.initGame(io, socket);   //console.log('client connected');
+});
+
+
+*/
